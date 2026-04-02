14bf4a2f32a093d4293fce59f899676f18a7d2e5
shaikayazhussain9121682625 <shaikayazhussain46@gmail.com>
2026-04-02 08:10:08 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,25 @@
+#Assignment Operators
+a = 10
+
+print("Initial value:", a)
+
+a += 5      # a = 10 + 5 = 15
+print("After += :", a)
+
+a -= 3      # 15 - 3 = 12
+print("After -= :", a)
+
+a *= 2      # 12 * 2 = 24
+print("After *= :", a)
+
+a /= 4      # 24 / 4 = 6.0
+print("After /= :", a)
+
+a %= 4      # 6 % 4 = 2
+print("After %= :", a)
+
+a //= 2     # 2 // 2 = 1
+print("After //= :", a)
+
+a **= 3     # 1 ** 3 = 1
+print("After **= :", a)
