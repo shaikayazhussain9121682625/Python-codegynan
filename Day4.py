412bec3aab3f7fc75a6e6b3980aec1373f128659
shaikayazhussain9121682625 <shaikayazhussain46@gmail.com>
2026-04-02 20:55:27 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,66 @@
+# # take input from user
+# n = int(input("Enter a number: "))
+
+# # print type
+# print("Data type of n:", type(n))
+
+# # print value
+# print("Value of n:", n)
+# x=int(input())
+# y=int(input())
+# c=complex(x,y)
+# print(type(c),c)
+
+# n=bool(input())
+# print(type(n),n)
+
+#1 one egg price is 5 rupess and wht is price of 'N' eggs price
+# Egg=6
+# n=int(input())
+# print(Egg*n)
+#2 if box contains N penicls and total price is M,then what price of each pencil price
+# n=int(input())
+# m=int(input())
+# print(m%n)
+
+#3 red ball price is 25
+#green ball price is 30
+#if i buy N-red ball and M-green balls,than what is the total cost
+# a=25
+# b=30
+# n=int(input())
+# x=int(input())
+# print(a*n+b*x)
+1.# n=0
+# if n == 0:
+#     print("Zero")
+# else:
+#     print("Non-Zero")
+# n = 5   # you can change this value
+
+2.# if n > 0:
+#     print("Positive Number")
+# else:
+#     print("Negative Number")
+
+#3
+# n = 6   # you can change this value
+
+# if n % 2 == 0:
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+#4
+# age = 18   # you can change this value
+
+# if age >= 18:
+#     print("Eligible for Voting")
+# else:
+#     print("Not Eligible for Voting")
+#5
+n=4
+if n%3==0:
+    print('divisibile by 3')
+else:
+    print("not divisbile by 3")
+    
