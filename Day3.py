fe94a9b02f271f8bb72019a0eeb4e8540c9e479d
shaikayazhussain9121682625 <shaikayazhussain46@gmail.com>
2026-04-02 08:12:47 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -1,6 +1,5 @@
 #Assignment Operators
 a = 10
-
 print("Initial value:", a)
 
 a += 5      # a = 10 + 5 = 15
@@ -23,3 +22,46 @@ print("After //= :", a)
 
 a **= 3     # 1 ** 3 = 1
 print("After **= :", a)
+
+# Logical Operators
+#1.AND
+a = 10
+print(a > 5 and a < 20)
+#2. OR
+a = 10
+print(a > 5 or a > 20)
+#3. NOT
+a = 10
+print(not(a > 5))
+
+#Relational Operators
+a = 10
+b = 5
+
+print("a == b :", a == b)   # Equal
+print("a != b :", a != b)   # Not equal
+print("a > b  :", a > b)    # Greater than
+print("a < b  :", a < b)    # Less than
+print("a >= b :", a >= b)   # Greater or equal
+print("a <= b :", a <= b)   # Less or equal
+
+#Membership Operators
+text = "python"
+nums = [1, 2, 3, 4]
+
+print("p" in text)       # True
+print("z" in text)       # False
+print("y" not in text)   # False
+
+print(2 in nums)         # True
+print(5 not in nums)     # True
+
+#Identity Operators
+x = 10
+y = 10
+z = 20
+
+print(x is y)        # True
+print(x is z)        # False
+print(x is not z)    # True
+
