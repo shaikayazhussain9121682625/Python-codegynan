c7c3db1ad8d46c1af56ebba10e49d592e0a55428
shaikayazhussain9121682625 <shaikayazhussain46@gmail.com>
2026-04-03 15:13:59 +0530

    Add files via upload

---
+++ Day5.py
@@ -0,0 +1,23 @@
+
+# marks = int(input("Enter marks: "))
+
+# if 90 <= marks <= 100:
+#     print('Grade A')
+# elif 80 <= marks <= 89:
+#     print('Grade B')
+# elif 60 <= marks <= 79:
+#     print('Grade C')
+# elif 40 <= marks <= 59:
+#     print('Grade D')
+# else:
+#     print('Fail')
+
+n=int(input('enter number'))
+if n>0 and n%2==0:
+    print('postive even')
+elif n<0 and n%2==0:
+    print('negative even')
+elif n>0 and n%2==1:
+    print('potive odd')
+elif n<0 and n%2==1:
+    print('negaive even')   
